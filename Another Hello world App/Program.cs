023ea67bccdb2d4d64ea29231bbf6e2b63bf03dd
Tanxy Rogue <tanxyrogue@gmail.com>
2017-09-09 22:25:04 +0200

    Created App Main Files

---
+++ Another Hello world App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Another_Hello_world_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
